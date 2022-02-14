d436d1200650b7a7aa3816db1deaada3174f5bbb
karuppasamythiyagu <ajsamy2156@gmail.com>
2022-02-14 12:53:01 +0530

    Some projects added

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Sample {
+	
+	private void StudentName() {
+		System.out.println("Student Name:Sinamika");
+
+	}
+	private void studentId() {
+		System.out.println("Student Id:KS0119");
+
+	}
+
+}
